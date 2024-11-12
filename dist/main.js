77a153de0efa6791a8cd8bb153d416af21dafa61
sushigadaisuki <osushi>
2024-11-13 06:03:13 +0900

    import に書き換え

--- dist/main.js
+++ dist/main.js
@@ -1,4 +1,5 @@
-var path = require("path");
-var url = require("url");
+import path from "path";
+import url from "url";
 const __dirname = path.dirname(url.fileURLToPath(import.meta.url));
-module.exports = { neologdDict: path.resolve(__dirname, "../dict")};
+//module.exports = { neologdDict: path.resolve(__dirname, "../dict")};
+export const neologdDict = path.resolve(__dirname, "../dict");
